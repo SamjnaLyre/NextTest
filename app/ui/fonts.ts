3f9e3e3deb83a1b80625a6faf35f911ecca928ec
samjnalyre <samjnalyre@gmail.com>
2024-03-07 23:48:16 +0900

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Comfortaa } from 'next/font/google';
+
+export const comfortaa = Comfortaa({ subsets: ['latin'], weight: ['400', '700'] });
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700']});
