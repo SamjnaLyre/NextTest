6c84cfd239aedd9b111b90b7d9990429b9442fa9
samjnalyre <samjnalyre@gmail.com>
2024-03-08 12:10:24 +0900

    authorization

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,13 +20,7 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -39,9 +33,6 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
-    console.log('Fetching latest invoices...');
-    await new Promise((resolve) => setTimeout(resolve, 1500));
-
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -53,8 +44,6 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
-
-    console.log('Data fetch completed after 1.5 seconds.');
 
     return latestInvoices;
   } catch (error) {
